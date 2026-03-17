f4b624947c107cf7f4bd040d0ca6fcb1d458a3a0
Janine Lüger <jaelma.lg@gmail.com>
2026-03-17 13:09:36 +0100

    AuthController erstellt, Routen aktualisiert

---
+++ src/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class AuthController {
+    /**
+     * 
+     */
+    public function login() {
+        echo 'You are logged in!';
+    }
+
+    public function register() {
+        echo 'Register here';
+    }
+}
--- src/routes.php
+++ src/routes.php
@@ -4,8 +4,10 @@ use App\Router;
 
 // namespaces to shorten ::class call
 use App\Controllers\HomeController;
+use App\Controllers\AuthController;
 
 /**
+ * Define routes, see pattern below. Will be passed through Router->add()
  * returns a function call of a Router object, either get() or post()
  */
 return function (Router $router): void {
@@ -18,4 +20,6 @@ return function (Router $router): void {
     */
 
     $router->add('GET', '/', HomeController::class, 'home');
+    $router->add('POST', '/login', AuthController::class, 'login');
+    $router->add('POST', '/register', AuthController::class, 'register');
 };
