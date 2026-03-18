db898d131ab495a407b902e881dba899a9aa9642
Janine Lüger <jaelma.lg@gmail.com>
2026-03-18 15:07:24 +0100

    seeds

--- db/seed.sql
+++ db/seed.sql
@@ -1,5 +1,10 @@
 USE quiz;
 
+INSERT INTO player (playername, password_hash, is_admin)
+VALUES
+    ('JEML', 'adkwoakdwao', false),
+    ('ADMIN', 'adminpassword', true);
+
 INSERT INTO difficulty (difficulty_label)
 VALUES
     ('Leicht'),
@@ -10,3 +15,21 @@ INSERT INTO category (category_label)
 VALUES
     ('Hauptstadt'),
     ('Geographie');
+
+INSERT INTO question (question_text, difficulty_id, category_id)
+VALUES
+    ('Deutschland', 1, 1),
+    ('Argentinien', 1, 1),
+    ('Brasilien', 1, 1),
+    ('China', 1, 1),
+    ('Japan', 1 , 1),
+    ('Belgien', 1, 1);
+
+INSERT INTO answer (answer_text, is_correct, question_id)
+VALUES
+    ('Berlin', 1, 1),
+    ('Buenos Aires', 1, 2),
+    ('Brasilia', 1, 3),
+    ('Peking', 1, 4),
+    ('Tokio', 1, 5),
+    ('Brüssel', 1, 6);
