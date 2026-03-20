6f7bb177c479bfd2e65467d226236f4c14195254
Janine Lüger <jaelma.lg@gmail.com>
2026-03-20 11:20:49 +0100

    better error handling

--- src/Views/register.php
+++ src/Views/register.php
@@ -3,10 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
     <title>Registrieren</title>
 </head>
 <body>
     <h1>Registrieren</h1>
+    
+    <?php
+    if (isset($_SESSION['errors'])) {
+        foreach ($_SESSION['errors'] as $e) {
+            echo "<p class='error'> $e </p>";
+        }
+
+        unset($_SESSION['errors']);
+    }
+    ?>
+
     <form action="/register" method="POST">
         <label>
             Spielername
