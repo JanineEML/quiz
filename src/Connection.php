48b6d33f3edb18020167601833f808d742192871
JanineEML <jaelma.lg@gmail.com>
2026-03-22 16:53:01 +0100

    implemented Singleton Pattern

--- src/Connection.php
+++ src/Connection.php
@@ -7,7 +7,7 @@ use PDOException;
 class Connection
 {
     /** @var PDO Singleton database connection instance. */
-    private static PDO $pdo;
+    private static ?PDO $pdo = null;
 
     /**
      * Singleton database connection.
@@ -16,15 +16,18 @@ class Connection
      * @return PDO
      * @throws PDOException If the connection fails.
      */
-    public static function connect(): PDO
+    public static function connect(): ?PDO
     {
         $dsn = 'mysql:host=mysql;port=3306;dbname=quiz;charset=utf8';
         $username = 'jeml';
         $password = 'pass';
 
         try {
-            self::$pdo = new PDO($dsn, $username, $password);
-            self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            if (is_null(self::$pdo)) {
+                self::$pdo = new PDO($dsn, $username, $password);
+                self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                }
+
         } catch (PDOException $e) {
             echo "Verbindung fehlgeschlagen" . $e->getMessage();
         }
