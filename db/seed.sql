afcd4ede6aeb4adf27634b2f1c734a05daeae9e0
JanineEML <jaelma.lg@gmail.com>
2026-03-22 22:18:54 +0100

    added categories

--- db/seed.sql
+++ db/seed.sql
@@ -8,23 +8,24 @@ VALUES
 
 INSERT INTO category (category_label)
 VALUES
-    ('Hauptstadt'),
-    ('Geographie');
+    ('Allgemeinwissen'),
 
-INSERT INTO question (question_text, difficulty_id, category_id)
-VALUES
-    ('Deutschland', 1, 1),
-    ('Argentinien', 1, 1),
-    ('Brasilien', 1, 1),
-    ('China', 1, 1),
-    ('Japan', 1 , 1),
-    ('Belgien', 1, 1);
+    -- WISSENSCHAFTEN
+    ('Geographie'),
+    ('Naturwissenschaft'),
+    ('Informatik'),
+    ('Mathematik'),
 
-INSERT INTO answer (answer_text, is_correct, question_id)
-VALUES
-    ('Berlin', 1, 1),
-    ('Buenos Aires', 1, 2),
-    ('Brasilia', 1, 3),
-    ('Peking', 1, 4),
-    ('Tokio', 1, 5),
-    ('Brüssel', 1, 6);
+    -- GAMING
+    ('Videospiel'),
+    ('Soulsborne'),
+
+    -- KULTUR
+    ('Film'),
+    ('Serie'),
+    ('Anime & Manga'),
+    ('Sprache'),
+    ('Essen & Trinken'),
+    ('Musik'),
+    ('Sport'),
+    ('Literatur');
