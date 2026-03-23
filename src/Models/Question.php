c74313d2fadfa60eb68f0803136786fd8e37dd60
Janine Lüger <jaelma.lg@gmail.com>
2026-03-23 16:43:58 +0100

    added fetchQuestion()

--- src/Models/Question.php
+++ src/Models/Question.php
@@ -7,10 +7,48 @@ class Question
 {
     /** @var PDO DB connection, shared with all model-queries. */
     private PDO $pdo;
-    
-    /** @param PDO $pdo Active database connection, provided by Connection::connect().*/
+
+    /** @param PDO $pdo Active database connection, provided by Connection::connect(). */
     public function __construct(PDO $pdo)
     {
         $this->pdo = $pdo;
+    }
+
+    /**
+     * 
+     */
+    public function fetchQuestions(?int $category, int $count): array
+    {
+        // Build statement
+        $tempStmt = "SELECT * FROM question";
+        
+        if ($category) {
+            $tempStmt .= " WHERE category_id = :category";
+        }
+
+        $tempStmt .= " ORDER BY RAND() LIMIT :count";
+
+        // Prepare statement, bind values
+        $stmt = $this->pdo->prepare($tempStmt);
+        $stmt->bindValue(':count', $count, PDO::PARAM_INT);
+
+        if ($category) {
+            $stmt->bindValue(':category', $category, PDO::PARAM_INT);
+        }
+
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        return $result;
+    }
+
+    /**
+     * Fetches all categories from the database, to fill the Dropdown Menu
+     */
+    public function fetchCategories(): array
+    {
+        $stmt = $this->pdo->query("SELECT category_label FROM category");
+
+        return $stmt->fetchAll(PDO::FETCH_COLUMN);
     }
 }
