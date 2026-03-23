76ae7b2c7f4914c8019f82b8ab74514500f4b51a
Janine Lüger <jaelma.lg@gmail.com>
2026-03-23 16:42:54 +0100

    changed to multiple .sql files

---
+++ db/insert.sql
@@ -0,0 +1,16 @@
+USE quiz;
+
+INSERT INTO difficulty (difficulty_label)
+VALUES
+    ('Leicht'),
+    ('Mittel'),
+    ('Schwer');
+
+INSERT INTO category (category_label)
+VALUES
+    ('Videospiel'),
+    ('Soulsborne'),
+    ('Fußball'),
+    ('Geographie'),
+    ('Unterhaltung'),
+    ('Allgemeinwissen');
