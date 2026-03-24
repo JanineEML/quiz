0140c3669873757eb2de5d5121645ed9c95b3712
Janine Lüger <jaelma.lg@gmail.com>
2026-03-24 15:10:36 +0100

    fixed main.css insertion

--- src/Views/quiz/play.php
+++ src/Views/quiz/play.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="/css/main.css">
     <title>Document</title>
 </head>
 <body>
+    <?php 
+    ?>
+
     <h1>Frage <?= $i + 1 ?> von <?= $total ?></h1>
 
     <?php
