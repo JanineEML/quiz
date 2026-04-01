77f2a7883f90581c7f5ffc733b77fdfc2cf3f39a
Janine Lüger <jaelma.lg@gmail.com>
2026-04-01 12:35:00 +0200

    added routes for Question Editing through AdminController

--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,7 @@ use App\Router;
 use App\Controllers\HomeController;
 use App\Controllers\AuthController;
 use App\Controllers\QuizController;
+use App\Controllers\AdminController;
 
 /** Defines all routes. Uses the add() function of Router to load the routes. */ 
 return function (Router $router): void {
@@ -17,19 +18,27 @@ return function (Router $router): void {
     */
 
     // HOME CONTROLLER
-    $router->add('GET', '/', HomeController::class, 'home');
+    $router->add('GET',  '/', HomeController::class, 'home');
 
     // AUTH CONTROLLER
-    $router->add('GET', '/login', AuthController::class, 'loginView');
-    $router->add('POST', '/login', AuthController::class, 'login');
-    $router->add('GET', '/register', AuthController::class, 'registerView');
+    $router->add('GET',  '/login',    AuthController::class, 'loginView');
+    $router->add('POST', '/login',    AuthController::class, 'login');
+    $router->add('GET',  '/register', AuthController::class, 'registerView');
     $router->add('POST', '/register', AuthController::class, 'register');
-    $router->add('POST', '/logout', AuthController::class, 'logout');
+    $router->add('POST', '/logout',   AuthController::class, 'logout');
 
     // QUIZ CONTROLLER
-    $router->add('GET', '/quiz/start', QuizController::class, 'startView');
-    $router->add('POST', '/quiz/start', QuizController::class, 'start');
-    $router->add('GET', '/quiz/play', QuizController::class, 'playView');
-    $router->add('POST', '/quiz/play', QuizController::class, 'play');
-    $router->add('GET', '/quiz/result', QuizController::class, 'resultView');
+    $router->add('GET',  '/quiz/start',  QuizController::class, 'startView');
+    $router->add('POST', '/quiz/start',  QuizController::class, 'start');
+    $router->add('GET',  '/quiz/play',   QuizController::class, 'playView');
+    $router->add('POST', '/quiz/play',   QuizController::class, 'play');
+    $router->add('GET',  '/quiz/result', QuizController::class, 'resultView');
+
+    // ADMIN CONTROLLER
+    $router->add('GET',  '/admin',                  AdminController::class, 'dashboard');
+    $router->add('GET',  '/admin/questions',        AdminController::class, 'questionsView');
+    $router->add('POST', '/admin/questions/add',    AdminController::class, 'addQuestion');
+    $router->add('GET',  '/admin/questions/edit',   AdminController::class, 'editQuestionView');
+    $router->add('POST', '/admin/questions/edit',   AdminController::class, 'editQuestion');
+    $router->add('POST', '/admin/questions/delete', AdminController::class, 'deleteQuestion');
 };
