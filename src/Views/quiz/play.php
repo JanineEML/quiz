4d89dd4c6c2e56b45e6a7d35fe2e5b1e10b88247
Janine Lüger <jaelma.lg@gmail.com>
2026-04-21 14:18:01 +0200

    added Timer

--- src/Views/quiz/play.php
+++ src/Views/quiz/play.php
@@ -1,14 +1,15 @@
 <?php require __DIR__ . '/../shared/header.php' ?>
 <div class="container-lg">
     <div class="row">
-        <div class="col">
+        <div class="col d-flex flex-column align-items-center">
             <h1>Frage <?= $i + 1 ?> von <?= $total ?></h1>
         
             <?php showErrors() ?>
         
-            <h2 class="text-center"><?= e($question['question_text']) ?></h2>
+            <div id="timer" class="mb-3 fs-3">30</div>
+            <p class="fs-3 text-center"><?= e($question['question_text']) ?></p>
         
-            <div class="quiz-answers">
+            <div class="quiz-answers mb-3">
                 <?php foreach ($answers as $a): ?>
                     <form action="/quiz/play" method="post">
                         <input type="hidden" name="answer_id" value="<?= $a['answer_id'] ?>">
@@ -23,7 +24,7 @@
 
             <?php $progress = (int) (($i) / $total * 100); ?>
 
-            <div class="progress">
+            <div class="progress w-100">
                 <div class="progress-bar"
                      role="progressbar"
                      style="width: <?= $progress ?>%"
@@ -42,8 +43,10 @@
     // ElementsByClass returns a HTML Collections, buttons needs to be cast into an array
     // with querySelectorAll this is not necessary
     const buttons = document.querySelectorAll(".quiz-answers button");
+    let timerId;
     buttons.forEach(button => {
         button.addEventListener("click", (event) => {
+            clearInterval(timerId);
             event.preventDefault();
 
             // Prevent changing the Answer by locking the Buttons
@@ -65,5 +68,16 @@
             setTimeout(() => {answer.closest("form").submit();}, 500);
         });
     });
+
+    let seconds = 30;
+    timerId = setInterval(() => {
+        seconds--;
+        document.querySelector("#timer").textContent = seconds;
+
+        if (seconds === 0) {
+            clearInterval(timerId);
+            document.querySelector(".quiz-answers form").submit();
+        }
+    }, 1000);
 </script>
 <?php require __DIR__ . '/../shared/footer.php' ?>
